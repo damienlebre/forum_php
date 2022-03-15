3455e6f4363eaa59371bd5ae9d848f1952b29cf9
Hysteresis <piballz@gmail.com>
2022-03-15 11:39:42 +0100

    connexiona la bdd

--- Models/Managers/DBManager.php
+++ Models/Managers/DBManager.php
@@ -1,6 +1,29 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "forum_php");
-if ($mysqli->connect_errno) {
-    echo "Échec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+
+class DbManager {
+    protected $bdd;
+    private $dbHost = "localhost";
+    private $dbName = "forum_php";
+    private $dbUser = "root";
+    private $dbPass = "";
+
+
+    public function __construct(){
+
+        $this->bdd = $this->connexionBdd() ;
+        
+    }
+
+    public function connexionBdd(){
+ 
+        $mysqli = new mysqli($this->dbHost, $this->dbUser , $this->dbPass, $this->dbName);
+        if ($mysqli->connect_errno) {
+            echo "Échec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        }
+        echo $mysqli->host_info . "\n";
+
+        return $mysqli;
+    }
+
 }
-echo $mysqli->host_info . "\n";
+
