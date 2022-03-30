6e43e887597b07aded6db52580ab99dd42388e9d
thorfin <cyrilgrangeon5@hotmail.com>
2022-03-30 14:03:42 +0200

    navbar ok if log or not

--- Views/navbar.php
+++ Views/navbar.php
@@ -1,3 +1,10 @@
+<?php
+
+@session_start();
+//var_dump($_SESSION['user']);
+
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,22 +15,33 @@
 
             <?php
                
-            //         echo('   <li class="nav-item">
-            //     <a class="nav-link" href="index.php?controller=security&action=logout">Me déconnecter</a>
-            // </li>
-            
-            //  <li class="nav-item">
-            //     <a class="nav-link" href="index.php?controller=question&action=list">Les questions !</a>
-            // </li
+       
+            if(isset($_SESSION['user'])){
+                echo('   <li class="nav-item">
+                      <a class="nav-link" href="index.php?controller=security&action=logout">Me déconnecter</a>
+                  </li>
+                
+                   <li class="nav-item">
+                      <a class="nav-link" href="index.php?controller=question&action=listQuestion">Les questions !</a>
+                  </li
 
-            //  <li class="float-end">Bonjour '.$this->user->getUsername().'</li>');
-                
-                    echo('   <li class="nav-item">
-                <a class="nav-link" href="index.php?controller=security&action=register">M\'enregistrer</a>
-            </li> <li class="nav-item">
-                <a class="nav-link" href="index.php?controller=security&action=login">Me connecter</a>
-            </li>');
-                
+                  <li class="nav-item">
+                      <a class="nav-link" href="index.php?controller=question&action=list">Mes questions !</a>
+                  </li
+    
+                    ');
+            
+                   
+        }else{
+            echo('   <li class="nav-item">
+            <a class="nav-link" href="index.php?controller=security&action=register">M\'enregistrer</a>
+        </li> <li class="nav-item">
+            <a class="nav-link" href="index.php?controller=security&action=login">Me connecter</a>
+        </li>
+        <li class="nav-item">
+              <a class="nav-link" href="index.php?controller=question&action=list">Les questions !</a>
+        </li');
+        }
             ?>
 
         </ul>
--- Views/question/listQuestions.php
+++ Views/question/listQuestions.php
@@ -2,7 +2,7 @@
 <head>
 <?php
 
-session_start();
+//session_start();
 
 ?>
 </head>
