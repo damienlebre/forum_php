4c372dc80cad60d3ba0d2b0fb9bf0b8b3a7f9bf9
thorfin <cyrilgrangeon5@hotmail.com>
2022-03-30 09:36:48 +0200

    tableau questions

--- Views/listQuestions.php
+++ Views/listQuestions.php
@@ -15,7 +15,7 @@
 
     var_dump($resultats->listQuestion());
     
-    die;
+    
     ?>
 
     <table class="table">
@@ -25,11 +25,41 @@
             <th scope="col">Question</th>
             <th scope="col">Date</th>
         </thead>
-<?php
-        foreach ($resultats as $resultat){
-            //toto fini moi cette merde de tableau !! CONARD
+    <?php
+        foreach ($resultats->listQuestion() as $resultat){
+            echo(' <tr>
+                <td>'.$resultat->getAuthor_user_id().'</td>
+                <td>'.$resultat->getSubject().'</td>
+                <td>'.$resultat->getContent().'</td>
+                <td>'.$resultat->getPublication().'</td>
+
+                <td>
+                    <a title="Supprimer"  href="index.php?controller=question&action=delete&id='.$resultat->getId().'">
+                        <i class="fa fa-trash"></i>
+                    </a>
+                    
+                       <a title="Détail"  href="index.php?controller=question&action=detail&id='.$resultat->getId().'">
+                        <i class="fa fa-eye"></i>
+                    </a>
+                    
+                      <a title="Editer"  href="index.php?controller=question&action=edit&id='.$resultat->getId().'">
+                        <i class="fa fa-edit"></i>
+                    </a>
+                </td>
+            </tr>');
         }
-
-
-
+        ?>
+         </tbody>
+    </table>
 </div>
+
+<script rel="script" src="Public/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+
+        
+
+
+
