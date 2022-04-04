872a27c109112d9de24261db6c02fa77b10bc4b3
damienlebre <lebre.damien88@gmail.com>
2022-04-04 16:19:41 +0200

    frontChap2

--- Views/question/addQuestion.php
+++ Views/question/addQuestion.php
@@ -8,9 +8,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Public\css\addQuestion.css">
     <title>Ajouter question</title>
 </head>
-    <body class="bg-success bg-gradient">
+    <body class="main-bg ">
         <h1 class="text-left ms-3 mb-5 text-white">Ajouter une question :</h1>
 
         <form  method="post" class="d-flex flex-column "> 
@@ -22,9 +23,9 @@
             <label for="content" class="ms-5 fw-bold text-white">Votre question :</label>
             <div class="ms-5">
                 <textarea name="content" id="content"  rows="10" class="col-6 rounded" required></textarea>
-            </div>
-            <button type="submit" class="btn btn-warning btn-sm mt-3 col-3 ms-5 text-white fw-bold">Soumettre</button>
-
+            </div>             
+                <button type="submit" class="btn btn-warning btn-sm mt-3 col-3 ms-5 text-white fw-bold ">Soumettre</button>
+            
             <?php
             if($errors != null){
                 foreach($errors as $error){
